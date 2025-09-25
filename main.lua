0b75df9de4e81126437bd674c5ab9ea389742ce9
Nyíri Dániel <nyiridaniel3@gmail.com>
2025-09-25 14:26:54 +0200

    Add camera and main character sprites

--- main.lua
+++ main.lua
@@ -1,31 +1,48 @@
 function love.load()
   anim8 = require 'lib/anim8'
+  sti = require 'lib/sti'
+  camera = require 'lib/camera'
 
   love.graphics.setDefaultFilter("nearest", "nearest")
 
+  -- player attributes
   player = {}
   player.x = love.graphics.getWidth() / 2
   player.y = love.graphics.getHeight() / 2
-  player.speed = 3
-  player.spriteSheet = love.graphics.newImage('res/sprites/main-sheet.png')
-  player.grid = anim8.newGrid( 12, 18, player.spriteSheet:getWidth(), player.spriteSheet:getHeight() )
+  player.speed = 2
+  player.idleSpriteSheet = love.graphics.newImage('res/sprites/main-character/main-character-idle-sword.png')
+  player.walkSpriteSheet = love.graphics.newImage('res/sprites/main-character/main-character-walk-sword.png')
+  player.idle = anim8.newGrid(32, 32, player.idleSpriteSheet:getWidth(), player.idleSpriteSheet:getHeight())
+  player.walk = anim8.newGrid(32, 32, player.walkSpriteSheet:getWidth(), player.walkSpriteSheet:getHeight())
 
+  -- player animations
   player.animations = {}
-  player.animations.down  = anim8.newAnimation( player.grid('1-4', 1), 0.2 )
-  player.animations.left  = anim8.newAnimation( player.grid('1-4', 2), 0.2 )
-  player.animations.right = anim8.newAnimation( player.grid('1-4', 3), 0.2 )
-  player.animations.up    = anim8.newAnimation( player.grid('1-4', 4), 0.2 )
+  player.animations.idle = anim8.newAnimation(player.idle('1-2', 1), 0.4)
 
+  player.animations.right = anim8.newAnimation(player.walk('1-4', 1), 0.2)
+  player.animations.left  = anim8.newAnimation(player.walk('1-4', 2), 0.2)
+  player.animations.upRight = anim8.newAnimation(player.walk('1-4', 3), 0.2)
+  player.animations.upLeft  = anim8.newAnimation(player.walk('1-4', 4), 0.2)
+  player.animations.downRight = anim8.newAnimation(player.walk('1-4', 1), 0.2)
+  player.animations.downLeft  = anim8.newAnimation(player.walk('1-4', 2), 0.2)
+
+  -- starting position
   player.anim = player.animations.right
+  player.lastHorizontal = "right"
+
+  -- loading map and camera
+  gameMap = sti('res/maps/map01/map01.lua')
+  cam = camera()
 end
 
 function love.update(dt)
   local isMoving = false
 
   if love.keyboard.isDown('d') then
-    if player.x < (love.graphics.getWidth()) then
+    if player.x < love.graphics.getWidth() then
       player.x = player.x + player.speed
       player.anim = player.animations.right
+      player.lastHorizontal = "right"
       isMoving = true
     end
   end
@@ -34,6 +51,7 @@ function love.update(dt)
     if player.x > 0 then
       player.x = player.x - player.speed
       player.anim = player.animations.left
+      player.lastHorizontal = "left"
       isMoving = true
     end
   end
@@ -41,26 +59,62 @@ function love.update(dt)
   if love.keyboard.isDown('w') then
     if player.y > 0 then
       player.y = player.y - player.speed
-      player.anim = player.animations.up
+      if player.lastHorizontal == "right" then
+        player.anim = player.animations.upRight
+      else
+        player.anim = player.animations.upLeft
+      end
       isMoving = true
     end
   end
 
   if love.keyboard.isDown('s') then
-    if player.y < (love.graphics.getHeight()) then
+    if player.y < love.graphics.getHeight() then
       player.y = player.y + player.speed
-      player.anim = player.animations.down
+      if player.lastHorizontal == "right" then
+        player.anim = player.animations.downRight
+      else
+        player.anim = player.animations.downLeft
+      end
       isMoving = true
     end
   end
 
-  if isMoving == false then
-    player.anim:gotoFrame(2)
+  if not isMoving then
+    player.anim = player.animations.idle
   end
 
   player.anim:update(dt)
+
+  cam:lookAt(player.x, player.y)
+
+  -- camera borders
+  local windowWidth = love.graphics.getWidth()
+  local windowHeight = love.graphics.getHeight()
+  local mapWidth = gameMap.width * gameMap.tilewidth
+  local mapHeight = gameMap.height * gameMap.tileheight
+
+  if cam.x < windowWidth/2 then cam.x = windowWidth/2 end
+  if cam.y < windowHeight/2 then cam.y = windowHeight/2 end
+  if cam.x > (mapWidth - windowWidth/2) then cam.x = (mapWidth - windowWidth/2) end
+  if cam.y > (mapHeight - windowHeight/2) then cam.y = (mapHeight - windowHeight/2) end
 end
 
 function love.draw()
-  player.anim:draw(player.spriteSheet, player.x, player.y, nil, 5)
+  cam:attach()
+    gameMap:drawLayer(gameMap.layers['terrain0'])
+    gameMap:drawLayer(gameMap.layers['terrain1'])
+    gameMap:drawLayer(gameMap.layers['walls'])
+    gameMap:drawLayer(gameMap.layers['stairs'])
+
+    if player.anim ~= player.animations.idle then
+      player.anim:draw(player.walkSpriteSheet, player.x, player.y, nil, 2, nil, 6, 9)
+    else
+      if player.lastHorizontal == "right" then
+        player.anim:draw(player.idleSpriteSheet, player.x, player.y, 0, 2, 2, 6, 9)
+      else
+        player.anim:draw(player.idleSpriteSheet, player.x, player.y, 0, -2, 2, 26, 9)
+      end
+    end
+  cam:detach()
 end
